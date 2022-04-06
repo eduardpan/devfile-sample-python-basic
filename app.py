d9ded58039ae5caae113144c704e6c0800189332
eduardpan <silviu.pantea@arobs.com>
2022-04-06 17:07:45 +0300

    add cities route

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def hello():
 def toys():
     return "My toys"
 
+@app.route('/cities')
+def cities():
+    return "My cities"
+
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
