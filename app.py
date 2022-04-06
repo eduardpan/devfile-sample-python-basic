0e2e9f4aa484a0ecc12786d4f967a9b3c7b87b39
Iulian Cozma <iulian.cozma@gmail.com>
2022-04-06 12:35:04 +0300

    fix

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def hello():
     return "Hello World from Python Training!"
 
 @app.route('/toys')
-def hello():
+def toys():
     return "My toys"
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8082)
