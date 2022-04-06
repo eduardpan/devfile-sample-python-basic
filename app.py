0b382fe5885768d1cb6f94542e3d615f8313f569
Iulian Cozma <iulian.cozma@gmail.com>
2022-04-06 12:28:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Python Training!"
 
+@app.route('/toys')
+def hello():
+    return "My toys"
+
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
